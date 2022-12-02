85753b7af636d17378b460d672eb4b13792ce908
Pelc314 <mpepelc@gmail.com>
2022-12-02 15:01:28 +0100

    more 7 kyu tasks

--- src/main/kotlin/Codewars.kt
+++ src/main/kotlin/Codewars.kt
@@ -1,18 +1,66 @@
-import java.math.BigInteger
 import java.math.RoundingMode
-import kotlin.math.floor
-import kotlin.math.roundToLong
-import kotlin.math.round
+import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 class Codewars() {
+    fun fizzBuzz(n: Int): Array<String> {
+        val resultList = mutableListOf<String>()
+        for (i in 0 until n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                resultList.add("FizzBuzz")
+            } else if (i % 5 == 0) {
+                resultList.add("Buzz")
+            } else if (i % 3 == 0) {
+                resultList.add("Fizz")
+            } else resultList.add(i.toString())
+        }
+        println(resultList)
+        return resultList.toTypedArray()
+    }
+
+    fun wallpaper(l: Double, w: Double, h: Double): String {
+        val sumOfm2Walls: Double = ((l * h) * 2 + (w * h) * 2) * 1.15
+        val oneRollM2: Double = 10 * 0.52
+        var result = sumOfm2Walls / oneRollM2
+
+        val numbers = listOf<String>(
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty"
+        )
+        if (w.toInt() == 0 || h.toInt() == 0 || l.toInt() == 0) result = 0.0
+        for (i in 0..numbers.size - 1) {
+            if (ceil(result).toInt() == i) return numbers[i]
+        }
+        return ceil(result).toString()
+    }
+
     fun londonCityHacker(journey: Array<Any>): String {
 
         var buses = journey.count { it is Int }
         var buses2: Double = 0.0
         if (buses % 2 != 0 && buses > 2) {
             buses2 = (((buses / 2) + 1) * 1.5)
-        } else if(buses>2) buses2 = (buses * 1.5) / 2
-        else buses2 = buses*1.5
+        } else if (buses > 2) buses2 = (buses * 1.5) / 2
+        else buses2 = buses * 1.5
 
         val tube = journey.count { it is String } * 2.4
 
@@ -21,3 +69,4 @@ class Codewars() {
     }
 
 }
+
--- src/test/kotlin/CodewarsClassTest.kt
+++ src/test/kotlin/CodewarsClassTest.kt
@@ -10,8 +10,11 @@ class CodewarsTest {
 
     @Test
     fun bigTest() {
-       assertEquals("£7.80",codeWars.londonCityHacker(arrayOf(12, "Central", "Circle", 21)));
-        assertEquals("£7.20",codeWars.londonCityHacker(arrayOf("Northern", "Central", "Circle")));
+        assertEquals("ten", codeWars.wallpaper(4.0, 3.5, 3.0))
+        assertEquals("sixteen", codeWars.wallpaper(6.3, 4.5, 3.29))
+        assertEquals("zero", codeWars.wallpaper(8.49, 9.16, 0.0))
+        assertEquals("£7.80", codeWars.londonCityHacker(arrayOf(12, "Central", "Circle", 21)));
+        assertEquals("£7.20", codeWars.londonCityHacker(arrayOf("Northern", "Central", "Circle")));
         assertEquals("£5.40", codeWars.londonCityHacker(arrayOf("Piccidilly", 56, 93, 243)));
 
     }
